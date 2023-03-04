69c16a1f59307d4339e1b24399fe230ee8693c1d
Sardor Abdullaev <abdullaevss94@mail.ru>
2023-03-05 02:53:15 +0500

    delete comment

--- script.js
+++ script.js
@@ -1,12 +1,6 @@
 const MAPBOX_ACCES_TOKEN='pk.eyJ1Ijoic2FyZG9yYWJkdWxsYWV2IiwiYSI6ImNrdjJhaWtxYzN3OXQyb3M3aGtwZnQ3MTEifQ.rcA__4l6dfAmkNrn3mlQKw'
 
 
-// var map = new mapboxgl.Map({
-//     accessToken:MAPBOX_ACCES_TOKEN,
-//   container: 'map',
-//   style: 'mapbox://styles/mapbox/streets-v11'
-// });
-
 navigator.geolocation.getCurrentPosition(successLocation,errorLocation,{enableHighAccuracy:true});
 
 function setupMap(centerPosition) {
